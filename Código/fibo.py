5dd44f6b0860950dddc54f8039eba77c319bb6e4
Paula <a16paulags@iessanclemente.net>
2023-11-23 10:51:39 +0100

    Añadida Función Mostrar Numeros desde Index==Posicion==1

--- Código/fibo.py
+++ Código/fibo.py
@@ -4,26 +4,28 @@
 """
 #f0=0 , f1=1 , fn=fn−1+fn−2
 
-def fibonacci(n=int(input("Introduce unha Posición:"))):
-
-  
-    if n < 0:
-        print("Sorry!!!, La Posición debe ser un Número Mayor que Cero")
-    
-    elif n == 0:   
-            
+# Defino a Función Fibonacci, xerará a secuencia de modo recursivo
+def fibonacci(n):
+      
+    if n == 0 or n == 1:               
         return n
        
-    elif n == 1:
-        return n
-        
     elif n >= 2:
         return ((fibonacci(n - 1) + fibonacci(n - 2)))
-   
-#chamamos á función
-# a ter en conta que as posicións comenzan en cero 
+    
+#Temos en conta que Función Fibonacci previa conta os indices dende Cero coma un array
+#Para facer coincidir a Posición co Indice creo Función no que gardamos en array a secuencia e mostramos a posición requerida
+def position(p=int(input("Introduce unha Posición:"))):
+  
+    if p <= 0:
+        print("Sorry!!!, La Posición debe ser un Número Mayor que Cero")
+    else:
+        posicion=[fibonacci(n) for n in range(p)]
+        return print ("Para la Posición", p, "el Número es:", posicion[-1])
+       
+
+#chamamos o programa principal
 
 if __name__ == '__main__':
-        print(fibonacci())
-   
- 
+    #print(position())
+    position()
